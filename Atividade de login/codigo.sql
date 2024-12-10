1b446457975e8face6f7d384c11c003ca241341e
carolineesouza <csgsilva1982@gmail.com>
2024-12-10 12:08:35 -0300

    Atividade de login

---
+++ Atividade de login/codigo.sql
@@ -0,0 +1,7 @@
+//* No meu banco de dados eu usei os seguintes comandos para fazer com que meu codigo funcionasse!*/
+
+CREATE DATABASE floricultura;
+CREATE TABLE usuario;
+CREATE TABLE produto;
+INSERT INTO usuarios (nome, email, senha) 
+VALUES ('carol', 'carol@carol.com', '123');
